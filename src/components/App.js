2db6c51c8bbf2219111e4f0d2a2b0b29cfee8eac
Afiz S <itsafiz@gmail.com>
2018-11-27 22:22:42 +0530

    Accessing nested data with props children

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import { Button } from "carbon-components-react";
+// import { Button } from "carbon-components-react";
 import Hello from "./Hello";
 
-const App = () => (
-  <div>
-    <Button>Hello Afiz!!</Button>
-    <Hello message="How Are you?" />
-  </div>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <Button>
+        I <Heart /> India
+      </Button>
+    );
+  }
+}
+
+const Button = props => <button>{props.children}</button>;
+
+class Heart extends React.Component {
+  render() {
+    return <span> &hearts;</span>;
+  }
+}
 
 export default App;
